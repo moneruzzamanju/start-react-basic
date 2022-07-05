5b32076b581764fe84685fb688e26f9c378e821d
moneruzzaman <moneruzzamanju@gmail.com>
2022-07-06 02:12:20 +0600

    Create Class Component named Greeting

---
+++ src/mycomponents/Greeting.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Greeting extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Greeting</h1>
+        <p>I am from Class Component.</p>
+        </div>
+    )
+  }
+}
