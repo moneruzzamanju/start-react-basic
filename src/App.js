bab372d33ba794759dcc28377c57e6b6def17934
moneruzzaman <moneruzzamanju@gmail.com>
2022-07-06 00:40:42 +0600

    Creat React Functional Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './mycomponents/Hello';
 
 function App() {
   return (
@@ -15,8 +16,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Moneruzzaman.
         </a>
+        <Hello></Hello>
+
       </header>
     </div>
   );
